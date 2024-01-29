fbc671ad42771a6c2b74c35ece77f37e04384f42
sdw-sh <108492624+not-an-ai@users.noreply.github.com>
2024-01-29 21:44:59 +0100

    Add time from compile time timestamp

--- ChouchouteLeChat.ino
+++ ChouchouteLeChat.ino
@@ -1,168 +1,194 @@
-#include <WiFi.h>
-// https://randomnerdtutorials.com/esp32-ntp-client-date-time-arduino-ide/
-// https://github.com/taranais/NTPClient
-#include <NTPClient.h>
-// https://www.upesy.com/blogs/tutorials/how-to-connect-wifi-acces-point-with-esp32
-#include <WiFiUdp.h>
+#include "unix_timestamp.h"
 
-#include "NetworkCredentials.h"
+#include <ESP32Time.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SPIFFS
 
-const char* ssid = SSID2;
-const char* password = PASSWORD2;
+#include "FS.h"
+#include "SPIFFS.h"
 
+/* You only need to format SPIFFS the first time you run a
+   test or else use the SPIFFS plugin to create a partition
+   https://github.com/me-no-dev/arduino-esp32fs-plugin */
+#define FORMAT_SPIFFS_IF_FAILED true
 
-const int ledPin = 16;
-const int relaisPin = 2;
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+bool fileExists(fs::FS &fs, const char * path){
+    Serial.printf("Checking if file %s exists\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("  It does not");
+        return false;
+    }
+    Serial.println("  It does");
+    return true;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ESP32Time rtc(60 * 60);
+
+const int ledPin = 33;
+const int relaisPin = 32;
 const int resistorPin = 34;
 int resistorValue = 0;
-int lastValue = 0;
+bool lastDetection = false;
 
 // Define NTP Client to get time
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+
 
 // Variables to save date and time
 String formattedDate;
 String dayStamp;
 String timeStamp;
 
-String get_wifi_status(int status){
-  switch(status){
-    case WL_IDLE_STATUS:
-    return "WL_IDLE_STATUS";
-    case WL_SCAN_COMPLETED:
-    return "WL_SCAN_COMPLETED";
-    case WL_NO_SSID_AVAIL:
-    return "WL_NO_SSID_AVAIL";
-    case WL_CONNECT_FAILED:
-    return "WL_CONNECT_FAILED";
-    case WL_CONNECTION_LOST:
-    return "WL_CONNECTION_LOST";
-    case WL_CONNECTED:
-    return "WL_CONNECTED";
-    case WL_DISCONNECTED:
-    return "WL_DISCONNECTED";
-  }
-}
-
 
 void setup() {
-  
-  pinMode(relaisPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
   Serial.begin(115200);
-  delay(1000);
-
   Serial.println("====================================================================================");
   Serial.println("==                                    STARTING                                    ==");
   Serial.println("====================================================================================");
-  Serial.println(__DATE__);
-  Serial.println(__TIME__);
-  Serial.println(__TIMESTAMP__);
 
-  /*
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(relaisPin, HIGH);
-    delay(i * 50);
-    digitalWrite(relaisPin, LOW); 
-    delay(i * 50);
+  rtc.setTime(__DATE_TIME_UNIX__ - 60 * 60); // it seems __DATE_TIME_UNIX__ is naive, not considering the offset, so I have to correct it 
+  Serial.println(rtc.getTime("%A, %B %d %Y %H:%M:%S"));
+
+  if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
+      Serial.println("SPIFFS Mount Failed");
+      return;
   }
-  */
 
- //// NTP ////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // Scan for networks
-  //while (true) {
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-      Serial.print(n);
-      Serial.println(" networks found");
-      for (int i = 0; i < n; ++i) {
-        // Print SSID and RSSI for each network found
-        Serial.print(i + 1);
-        Serial.print(": ");
-        Serial.print(WiFi.SSID(i));
-        Serial.print(" (");
-        Serial.print(WiFi.RSSI(i));
-        Serial.print(")");
-        Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-        delay(10);
-      }
-    }
-    delay(1000);
-  //}
-
-  Serial.println("--- SSID: ---");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  int status = WL_IDLE_STATUS;
-  int timeout_counter = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    status = WiFi.status();
-    delay(500);
-    Serial.println(get_wifi_status(status));
-    timeout_counter++;
-    if(timeout_counter >= 300 || status == WL_NO_SSID_AVAIL){
-      ESP.restart();
-    }
-    //Serial.print(".");
+  if (!fileExists(SPIFFS, "/leChat.log")) {
+    writeFile(SPIFFS, "/leChat.log", "Start of log\r\n");
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+
+  listDir(SPIFFS, "/", 0);
+
+  Serial.println("------------------------------------------------------------------------------------");
+  readFile(SPIFFS, "/leChat.log");
+  Serial.println("------------------------------------------------------------------------------------");
+
+  appendFile(SPIFFS, "/leChat.log", rtc.getTime("%A, %B %d %Y %H:%M:%S").c_str());
+  appendFile(SPIFFS, "/leChat.log", "  |  Appareil démarré.\r\n");
+
+  pinMode(relaisPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  delay(5000);
 
   digitalWrite(ledPin, LOW);
-
-  // Initialize a NTPClient to get time
-  timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  // timeClient.setTimeOffset(3600);
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
+
 void loop() {
-
-  while(!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
-
-    // The formattedDate comes with the following format:
-  // 2018-05-28T16:00:13Z
-  // We need to extract date and time
-  formattedDate = timeClient.getFormattedDate();
-  Serial.println(formattedDate);
-
-  // Extract date
-  int splitT = formattedDate.indexOf("T");
-  dayStamp = formattedDate.substring(0, splitT);
-  Serial.print("DATE: ");
-  Serial.println(dayStamp);
-  // Extract time
-  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
-  Serial.print("HOUR: ");
-  Serial.println(timeStamp);
-
   resistorValue = analogRead(resistorPin);
-  if (resistorValue > 1000) { //lastValue > resistorValue + 50 || lastValue < resistorValue - 50 
+  if (resistorValue > 1000) { 
+    if (!lastDetection) {
+      appendFile(SPIFFS, "/leChat.log", rtc.getTime("%A, %B %d %Y %H:%M:%S").c_str());
+      appendFile(SPIFFS, "/leChat.log", "  |  Le chat est arrivé.\r\n");
+      lastDetection = true;
+    }
     digitalWrite(relaisPin, HIGH); 
+    digitalWrite(ledPin, HIGH); 
   } else {
+    if (lastDetection) {
+      appendFile(SPIFFS, "/leChat.log", rtc.getTime("%A, %B %d %Y %H:%M:%S").c_str());
+      appendFile(SPIFFS, "/leChat.log", "  |  Le chat est parti.\r\n");
+      lastDetection = false;
+    }
     digitalWrite(relaisPin, LOW); 
+    digitalWrite(ledPin, LOW); 
   }
-  lastValue = resistorValue;
   Serial.println(resistorValue);
-  delay(1000);
+  delay(5000);
 }
 
 // https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
